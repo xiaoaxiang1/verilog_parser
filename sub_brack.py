5ee953ceb2ec5e0c88eeb1e355b3cc3a063b0eb6
xiaoaxiang1 <zwx96@qq.com>
2025-05-12 00:29:12 +0800

    update grammar

--- sub_brack.py
+++ sub_brack.py
@@ -1,5 +1,17 @@
 import re
 
+
+def replace_braces(line):
+    pattern = r'\{([^{}]*)\}'
+    matches = re.findall(pattern, line)
+
+    if matches:
+        line = re.sub(pattern, r'(\1)*', line)
+        return replace_braces(line)
+    else:
+        return line
+
+
 with open("./new_grammar.py", "r") as f:
     grammar_line_list = f.readlines()
 
@@ -14,5 +26,24 @@ for i, line in enumerate(grammar_line_list):
         continue
 
     if re.search(r"\{\s*(\w+)\s*\}", line):
-        new_line = re.sub(r"\{\s*(\w+)\s*\}", r"\1*", line)
-    elif re.search(r"")
+        line = re.sub(r"\{\s*(\w+)\s*\}", r"\1*", line)
+    
+    if re.search(r'"\{"', line):
+        line = re.sub(r'"\{"', r'"lbraces"', line)
+	
+    if re.search(r'"\}"', line):
+        line = re.sub(r'"\}"', r'"rbraces"', line)
+	
+    line = replace_braces(line)
+
+    if re.search(r'"lbraces"', line):
+        line = re.sub(r'"lbraces"', '"{"', line)
+	
+    if re.search(r'"rbraces"', line):
+        line = re.sub(r'"rbraces"', '"}"', line)
+
+    new_grammar_line_list.append(line)
+
+with open("./new_grammar_edit.py", "w+")  as f:
+    f.writelines(new_grammar_line_list)
+#print(new_grammar_line_list)
